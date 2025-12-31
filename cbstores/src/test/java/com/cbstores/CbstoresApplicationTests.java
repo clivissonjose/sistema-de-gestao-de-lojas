506f281d136383f3531bb2f116cb1c9368e4f920
Clivisson <cjbarbosa2001@gmail.com>
2025-12-31 15:57:58 -0300

    feat: spring boot initilizer

---
+++ cbstores/src/test/java/com/cbstores/CbstoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbstores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbstoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
